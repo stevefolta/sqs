e805d397db83dfd39a3d2e14199f977f86c11ada
Steve Folta <steve@folta.net>
2023-05-07 16:07:18 -0700

    print(): Can print things that aren't strings.

--- Print.c
+++ Print.c
@@ -2,6 +2,7 @@
 #include "String.h"
 #include "Object.h"
 #include "Dict.h"
+#include "ByteCode.h"
 #include "Error.h"
 #include <stdio.h>
 
@@ -20,10 +21,8 @@ struct Object* Print(struct Object* self, struct Object** args)
 		}
 
 	if (args[0]) {
-		if (args[0]->class_ != &String_class) {
-			//*** TODO: call "string" method.
-			Error("Attempt to print a non-string.");
-			}
+		if (args[0]->class_ != &String_class)
+			args[0] = call_method(args[0], new_c_static_String("string"), NULL);
 		String* str = (String*) args[0];
 		fwrite(str->str, str->size, 1, stdout);
 		}
