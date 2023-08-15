de750220ebf60f9fb58275bced8a8e320bb1e043
Steve Folta <steve@folta.net>
2023-08-15 01:58:16 -0700

    env.as-dict.

--- Env.c
+++ Env.c
@@ -1,8 +1,10 @@
 #include "Env.h"
 #include "String.h"
+#include "Dict.h"
 #include "Class.h"
 #include "Object.h"
 #include <stdlib.h>
+#include <string.h>
 
 Class Env_class;
 Env env_obj = { &Env_class };
@@ -15,6 +17,22 @@ Object* Env_at(Object* super, Object** args)
 	return (result ? (Object*) new_c_String(result) : NULL);
 }
 
+Object* Env_as_dict(Object* super, Object** args)
+{
+	Dict* dict = new_Dict();
+	extern char** environ;
+	for (char** e = environ; *e; ++e) {
+		const char* entry = *e;
+		char* delim = strchr(entry, '=');
+		if (delim == NULL)
+			continue;
+		String* key = new_String(entry, delim - entry);
+		String* value = new_c_String(delim + 1);
+		Dict_set_at(dict, key, (Object*) value);
+		}
+	return (Object*) dict;
+}
+
 
 void Env_init_class()
 {
@@ -22,6 +40,7 @@ void Env_init_class()
 
 	static const BuiltinMethodSpec builtin_methods[] = {
 		{ "[]", 1, Env_at },
+		{ "as-dict", 0, Env_as_dict },
 		{ NULL },
 		};
 	Class_add_builtin_methods(&Env_class, builtin_methods);
