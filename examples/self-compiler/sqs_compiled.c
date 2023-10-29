ba88409f2d3d2ad065120c173265de87bda1aa0b
Steve Folta <steve@folta.net>
2023-10-29 12:07:43 -0700

    examples/self-compiler.

---
+++ examples/self-compiler/sqs_compiled.c
@@ -0,0 +1,143 @@
+#include "sqs_compiled.h"
+#include "LinesIterator.h"
+#include "Error.h"
+#include <string.h>
+
+
+UpvalFrame* cur_upval_frame_ = NULL;
+Array argv_obj = { &Array_class, 0, 0, NULL };
+
+
+Object* call_(const char* name, Object* receiver, int num_args, Object** args)
+{
+	// Find the method.
+	String name_str;
+	String_init_static_c(&name_str, name);
+	Object* method = Object_find_method(receiver, &name_str);
+	if (method == NULL || method->class_ != &BuiltinMethod_class) {
+		Class* receiver_class = (receiver ? receiver->class_ : &Nil_class);
+		Error("Unhandled method call: \"%s\" on %s.", name, String_c_str(receiver_class->name));
+		}
+
+	// Call it.
+	BuiltinMethod* builtin_method = (BuiltinMethod*) method;
+	int method_num_args = builtin_method->num_args;
+	if (num_args < method_num_args) {
+		Object* more_args[method_num_args];
+		memset(more_args, 0, sizeof(more_args));
+		int i = 0;
+		for (; i < num_args; ++i)
+			more_args[i] = args[i];
+		for (; i < method_num_args; ++i)
+			more_args[i] = NULL;
+		return builtin_method->fn(receiver, more_args);
+		}
+	else
+		return builtin_method->fn(receiver, args);
+}
+
+
+Object* super_call_(const char* name, Object* receiver, int num_args, Object** args)
+{
+	// Find the method.
+	String name_str;
+	String_init_static_c(&name_str, name);
+	Object* method = Object_find_super_method(receiver, &name_str);
+	if (method == NULL || method->class_ != &BuiltinMethod_class) {
+		Class* receiver_class = (receiver ? receiver->class_ : &Nil_class);
+		Error("Unhandled method call: \"%s\" on %s.", name, String_c_str(receiver_class->name));
+		}
+
+	// Call it.
+	BuiltinMethod* builtin_method = (BuiltinMethod*) method;
+	int method_num_args = builtin_method->num_args;
+	if (num_args < method_num_args) {
+		Object* more_args[method_num_args];
+		memset(more_args, 0, sizeof(more_args));
+		int i = 0;
+		for (; i < num_args; ++i)
+			more_args[i] = args[i];
+		for (; i < method_num_args; ++i)
+			more_args[i] = NULL;
+		return builtin_method->fn(receiver, more_args);
+		}
+	else
+		return builtin_method->fn(receiver, args);
+}
+
+
+Object** get_upvalue_(int capture_id, int index)
+{
+	for (UpvalFrame* frame = cur_upval_frame_; frame; frame = frame->up) {
+		if (frame->capture_id == capture_id)
+			return frame->captures[index];
+		}
+	Error("Internal error: bad capture ID (%d)", capture_id);
+}
+
+Object* call_object(Object* receiver, String* name, Array* args)
+{
+	// Find the method.
+	Object* method = Object_find_method(receiver, name);
+	if (method == NULL || method->class_ != &BuiltinMethod_class) {
+		Class* receiver_class = (receiver ? receiver->class_ : &Nil_class);
+		Error("Unhandled method call: \"%s\" on %s.", name, String_c_str(receiver_class->name));
+		}
+
+	// Call it.
+	BuiltinMethod* builtin_method = (BuiltinMethod*) method;
+	int num_args = (args ? args->size : 0);
+	int method_num_args = builtin_method->num_args;
+	if (num_args < method_num_args) {
+		Object* more_args[method_num_args];
+		memset(more_args, 0, sizeof(more_args));
+		int i = 0;
+		for (; i < num_args; ++i)
+			more_args[i] = args->items[i];
+		for (; i < method_num_args; ++i)
+			more_args[i] = NULL;
+		return builtin_method->fn(receiver, more_args);
+		}
+	else
+		return builtin_method->fn(receiver, (args ? args->items : NULL));
+}
+
+
+static void init_all()
+{
+	Class_init_class();
+	Object_init_class();
+	String_init_class();
+	Int_init_class();
+	Boolean_init_class();
+	Array_init_class();
+	ByteArray_init_class();
+	Dict_init_class();
+	BuiltinMethod_init_class();
+	Nil_init_class();
+	File_init_class();
+	Pipe_init_class();
+	LinesIterator_init_class();
+	Regex_init_class();
+	Path_init_class();
+	Env_init_class();
+	Run_init();
+}
+
+
+int main(int argc, char* argv[])
+{
+	init_all();
+	extern void sqs_init_classes();
+	sqs_init_classes();
+
+	for (int i = 0; i < argc; ++i)
+		Array_append(&argv_obj, (Object*) new_c_static_String(argv[i]));
+	extern Object* sqs_main(Object* self, Object** args);
+	Object* result = sqs_main(NULL, NULL);
+	if (result)
+		return Int_enforce(result, "main result");
+	return 0;
+}
+
+
---
+++ examples/self-compiler/sqs_compiled.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "Object.h"
+#include "Class.h"
+#include "String.h"
+#include "Int.h"
+#include "Array.h"
+#include "Dict.h"
+#include "Boolean.h"
+#include "ByteArray.h"
+#include "Nil.h"
+#include "File.h"
+#include "Pipe.h"
+#include "Path.h"
+#include "BuiltinMethod.h"
+
+#include "Print.h"
+#include "Run.h"
+#include "Regex.h"
+#include "Glob.h"
+#include "MiscFunctions.h"
+#include "Fail.h"
+#include "Env.h"
+
+
+typedef Object* Object_ptr;
+
+#define ivar_get_(index) ( ((Object**) self)[index + 1] )
+#define ivar_set_(index, value) ( ((Object**) self)[index + 1] = value )
+
+extern Object* call_(const char* name, Object* receiver, int num_args, Object** args);
+extern Object* super_call_(const char* name, Object* receiver, int num_args, Object** args);
+
+typedef struct UpvalFrame UpvalFrame;
+struct UpvalFrame {
+	int capture_id;
+	UpvalFrame* up;
+	Object*** captures;
+	};
+extern Object** get_upvalue_(int capture_id, int index);
+extern UpvalFrame* cur_upval_frame_;
+
+extern Array argv_obj;
+
+#define inline_string_(value) ((Object*) &(String) { &String_class, value, sizeof(value) - 1 })
+
